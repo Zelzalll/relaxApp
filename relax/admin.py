5f3a38958bb4be2e59219dfbe834ae444ba8acad
bibichanel <bibichanel@kali.localdomain>
2021-10-18 05:40:07 -0400

    Frist commit

---
+++ relax/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+"""
+    Tài khoản admin super:
+    user: admin
+    pass: adminrelax
+"""
---
+++ relax/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def test(request):
+    context = {
+        "name": "Khánh Trung", 
+        "age": 20, 
+        "email": "lytrung15901@gmail.com"
+        }
+    return render(request, "relax/test.html", context)
+
